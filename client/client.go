9e7a9e0a54ae58fee3486e9e4ff0ebcc28d3b58c
anxielray <anxielworld@gmil.com>
2024-12-08 16:24:33 +0300

    [Improvement]: Add the options that allow a client to exit from the chat inbox

--- client/client.go
+++ client/client.go
@@ -3,26 +3,31 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
+	"strings"
 )
 
 func Client() {
-
-	//set the port
-	port := os.Getenv("SERVER_PORT")
-	if port == "" {
-		port = "8080"
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("dial error", err)
+		return
 	}
-	conn, err := net.Dial("tcp", "localhost:"+port)
-	fmt.Println("enter message")
+	defer conn.Close()
+
+	fmt.Println("Enter message (type 'exit' to quit):")
 	message := ""
 	for {
 
+		//allow the user to exit from the
 		fmt.Scan(&message)
-		if err != nil {
-			fmt.Println("dial error", err)
+		if message == "exit" || message == "quit" || strings.HasPrefix(message, "bye") {
+			break
 		}
-		conn.Write([]byte(message))
+		_, err := conn.Write([]byte(message))
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			return
+		}
 	}
 }
 
